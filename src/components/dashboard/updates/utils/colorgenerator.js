dfaf49ca6ec4283bfa570da5baa6631c6bf4edc0
SanRaph <raphytex@gmail.com>
2020-11-10 13:22:04 +0100

    add changes to updates folder a colorGenerator function

---
+++ src/components/dashboard/updates/utils/colorgenerator.js
@@ -0,0 +1,9 @@
+
+function colorGenerator(){
+    let colorCode = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' +
+    (Math.floor(Math.random() * 256)) + ',' +
+    (Math.floor(Math.random() * 256)) + ')';
+
+}
+
+export default colorGenerator
